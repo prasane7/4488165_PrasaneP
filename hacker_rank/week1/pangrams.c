500319bf6b0b021b1857ad7f556f2e5f42ec2c40
prasane7 <prasanepantala7@gmail.com>
2025-08-09 16:20:02 +0530

    Hacker rank solution

---
+++ hacker_rank/week1/pangrams.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char* pangrams(char* s) {
+    int present[26] = {0};
+    int count = 0;
+    char* answer;
+
+    for (int i = 0; s[i] != '\0'; ++i) {
+        if (isalpha(s[i])) {
+            int idx = tolower(s[i]) - 'a';
+            if (present[idx] == 0) {
+                present[idx] = 1;
+                count++;
+            }
+        }
+    }
+
+    if (count == 26) {
+        answer = (char*)malloc(8 * sizeof(char));
+        strcpy(answer, "pangram");
+    } else {
+        answer = (char*)malloc(12 * sizeof(char));
+        strcpy(answer, "not pangram");
+    }
+
+    return answer;
+}
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    // Remove trailing newline if any
+    str[strcspn(str, "\n")] = '\0';
+
+    char* result = pangrams(str);
+    printf("%s\n", result);
+    free(result);
+
+    return 0;
+}
